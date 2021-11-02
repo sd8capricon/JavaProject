6fc8a3c6c37689559ff9309925d2c27e718614e6
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-11-02 21:57:31 +0530

    changes from boris

--- Form.java
+++ Form.java
@@ -4,13 +4,14 @@ import java.io.File;
 import java.io.FileWriter;
 
 public class Form extends JFrame implements ActionListener {
-    JLabel nameL, mobileL, genderL, dobL, addressL, msgL;
-    JTextField nameTF, mobileTF;
+    JLabel nameL, mobileL, genderL, dobL, addressL, msgL,emailL,examL,marksL,branchL,collegeL;
+    JTextField nameTF, mobileTF, emailTF,markTF;
     JTextArea addressTA, dispTA;
     JRadioButton maleRB, femaleRB;
-    JComboBox dayCB, monthCB, yearCB;
+    JComboBox dayCB, monthCB, yearCB,examCB,branchCB;
     JCheckBox termsChB;
     JButton submitB;
+    
 
     String days[] = new String[31];
     String months[] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec" };
@@ -31,38 +32,53 @@ public class Form extends JFrame implements ActionListener {
 
     Form() {
         initNums();
-        setTitle("Registration Form");
-        setSize(700, 500);
+        setTitle("Student Registration Form");
+        setSize(700, 700);
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        //College Name
+        collegeL = new JLabel("DON BOSCO INSTITUTE OF TECHNOLOGY");
+        collegeL.setBounds(220,50,300,30);
+        add(collegeL);
+
         // Name Section
         nameL = new JLabel("Name");
-        nameL.setBounds(20, 50, 100, 20);
+        nameL.setBounds(20, 100, 100, 20);
         add(nameL);
 
         nameTF = new JTextField();
-        nameTF.setBounds(130, 50, 100, 20);
+        nameTF.setBounds(130, 100, 100, 20);
         add(nameTF);
 
         // Contact Section
         mobileL = new JLabel("Mobile");
-        mobileL.setBounds(20, 100, 100, 20);
+        mobileL.setBounds(20, 150, 100, 20);
         add(mobileL);
 
         mobileTF = new JTextField();
-        mobileTF.setBounds(130, 100, 100, 20);
+        mobileTF.setBounds(130, 150, 100, 20);
         add(mobileTF);
+
+        // Email Section
+        emailL = new JLabel("Email ID");
+        emailL.setBounds(20, 200, 100, 20);
+        add(emailL);
+
+        emailTF = new JTextField();
+        emailTF.setBounds(130, 200, 100, 20);
+        add(emailTF);
+
 
         // Gender Section
         genderL = new JLabel("Gender");
-        genderL.setBounds(20, 150, 100, 20);
+        genderL.setBounds(20, 250, 100, 20);
         add(genderL);
 
         maleRB = new JRadioButton("Male");
         femaleRB = new JRadioButton("Female");
-        maleRB.setBounds(130, 150, 80, 20);
-        femaleRB.setBounds(220, 150, 100, 20);
+        maleRB.setBounds(130, 250, 80, 20);
+        femaleRB.setBounds(220, 250, 100, 20);
         add(maleRB);
         add(femaleRB);
 
@@ -72,47 +88,76 @@ public class Form extends JFrame implements ActionListener {
 
         // DOB Section
         dobL = new JLabel("DOB");
-        dobL.setBounds(20, 200, 100, 20);
+        dobL.setBounds(20, 300, 100, 20);
         add(dobL);
 
         dayCB = new JComboBox(days);
         monthCB = new JComboBox(months);
         yearCB = new JComboBox(years);
-        dayCB.setBounds(130, 200, 50, 20);
-        monthCB.setBounds(190, 200, 50, 20);
-        yearCB.setBounds(250, 200, 60, 20);
+        dayCB.setBounds(130, 300, 50, 20);
+        monthCB.setBounds(190, 300, 50, 20);
+        yearCB.setBounds(250, 300, 60, 20);
         add(dayCB);
         add(monthCB);
         add(yearCB);
 
         // Address Section
         addressL = new JLabel("Address");
-        addressL.setBounds(20, 250, 100, 20);
+        addressL.setBounds(20, 350, 100, 20);
         add(addressL);
 
         addressTA = new JTextArea();
-        addressTA.setBounds(130, 240, 200, 50);
+        addressTA.setBounds(130, 340, 200, 50);
         add(addressTA);
+
+        // Exam Section
+        examL = new JLabel("Exam");
+        examL.setBounds(20,400,100,20);
+        add(examL);
+
+        String examb[] = {"JEE","MHT-CET"};
+        examCB = new JComboBox(examb);
+        examCB.setBounds(130,400,100,20);
+        add(examCB);
+
+        // Marks Section
+        marksL = new JLabel("Marks");
+        marksL.setBounds(20,450,100,20);
+        add(marksL);
+
+        markTF = new JTextField();
+        markTF.setBounds(130, 450, 100, 20);
+        add(markTF);
+
+        // Branch Section
+        branchL = new JLabel("Branch");
+        branchL.setBounds(20,500,100,20);
+        add(branchL);
+
+        String branchb[] = {"COMPUTER SCIENCE","INFORMATION TECHNOLOGY","EXTC","MECHANICAL"};
+        branchCB = new JComboBox(branchb);
+        branchCB.setBounds(130,500,200,20);
+        add(branchCB);
 
         // Terms Section
         termsChB = new JCheckBox("I Accept Terms and Condition");
-        termsChB.setBounds(50, 300, 250, 20);
+        termsChB.setBounds(250, 550, 250, 20);
         add(termsChB);
 
         // Submit Button
         submitB = new JButton("Submit");
-        submitB.setBounds(150, 350, 80, 20);
+        submitB.setBounds(300, 600, 80, 20);
         submitB.addActionListener(this);
         add(submitB);
 
         // Display Area
         dispTA = new JTextArea();
-        dispTA.setBounds(350, 50, 300, 300);
+        dispTA.setBounds(350, 100, 300, 400);
         add(dispTA);
 
         // MSG
         msgL = new JLabel("");
-        msgL.setBounds(2, 400, 250, 20);
+        msgL.setBounds(50, 600, 250, 20);
         add(msgL);
 
         setVisible(true);
@@ -122,12 +167,16 @@ public class Form extends JFrame implements ActionListener {
         if (termsChB.isSelected()) {
             String name = nameTF.getText();
             String num = mobileTF.getText();
+            String email = emailTF.getText();
+            String exam = (String)(examCB.getSelectedItem());
+            String marks = markTF.getText();
+            String branch = (String)branchCB.getSelectedItem();
             String gender = femaleRB.isSelected() ? "Female" : "Male";
             String dob = dayCB.getSelectedItem() + "-" + monthCB.getSelectedItem() + "-" + yearCB.getSelectedItem();
             String address = addressTA.getText();
             
-            String data = "Name: " + name + "\nMobile Number: " + num + "\nGender: " + gender + "\nDOB: " + dob
-                    + "\nAddress: " + address;
+            String data = "Name: " + name + "\nMobile Number: " + num+"\nEmail ID: "+ email + "\nGender: " + gender + "\nDOB: " + dob
+                    + "\nAddress: " + address +"\nExam: " + exam  + "\nMarks: " + marks + "\nBranch: " + branch;
             dispTA.setText(data);
             
             try {
